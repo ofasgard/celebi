f235513f4630d3872478588d524204d9583da6a3
Callum Murphy-Hale <19550999+ofasgard@users.noreply.github.com>
2026-03-05 18:39:43 +0000

    feat(PIC)!: Basic test of PICO functionality
    This breaks the implant! It intentionally messes up the payload UUID to confirm that the PICO is working as intended...

--- celebi/celebi_pic/src/pico/checkin.c
+++ celebi/celebi_pic/src/pico/checkin.c
@@ -1,7 +1,9 @@
 #include <windows.h>
 #include "../headers/celebi.h"
-#include "../headers/tcg.h"
 
 void go(CheckinRequest *req) {
-	dprintf("Checkin PICO is loaded!");
+	req->payload_uuid[0]= 0x41;
+	req->payload_uuid[1]= 0x42;
+	req->payload_uuid[2]= 0x43;
+	req->payload_uuid[3]= 0x44;
 }
