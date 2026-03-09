207d51f3c22d29b6b90335fd92c414b57c35ee38
Callum Murphy-Hale <19550999+ofasgard@users.noreply.github.com>
2026-03-09 12:18:06 +0000

    style(Agent): Spaces to tabs

--- celebi/celebi_agent/exit.py
+++ celebi/celebi_agent/exit.py
@@ -2,32 +2,32 @@ from mythic_container.MythicCommandBase import *
 
 class ExitArguments(TaskArguments):
 
-    def __init__(self, command_line, **kwargs):
-        super().__init__(command_line, **kwargs)
-        self.args = []
+	def __init__(self, command_line, **kwargs):
+		super().__init__(command_line, **kwargs)
+		self.args = []
 
-    async def parse_arguments(self):
-        if len(self.command_line) > 0:
-            raise Exception("Exit command takes no parameters.")
+	async def parse_arguments(self):
+		if len(self.command_line) > 0:
+			raise Exception("Exit command takes no parameters.")
 
 class ExitCommand(CommandBase):
-    cmd = "exit" # Name of the command
-    help_cmd = "exit" # Help information presented to the user
-    argument_class = ExitArguments # The class used for processing & validating arguments
-    description = "Tasks the agent to clean up resources and exit."
-    needs_admin = False
-    version = 1
-    author = "@ofasgard"
-    attackmapping = []
-    supported_ui_features = ["callback_table:exit"]
-    attributes = CommandAttributes(
-        builtin=True, # Is this command always compiled into this payload type?
-        suggested_command=True, # Is this command preselected when building a payload?
-    )
+	cmd = "exit" # Name of the command
+	help_cmd = "exit" # Help information presented to the user
+	argument_class = ExitArguments # The class used for processing & validating arguments
+	description = "Tasks the agent to clean up resources and exit."
+	needs_admin = False
+	version = 1
+	author = "@ofasgard"
+	attackmapping = []
+	supported_ui_features = ["callback_table:exit"]
+	attributes = CommandAttributes(
+		builtin=True, # Is this command always compiled into this payload type?
+		suggested_command=True, # Is this command preselected when building a payload?
+	)
 
-    async def create_go_tasking(self, taskData: PTTaskMessageAllData) -> PTTaskCreateTaskingMessageResponse:
-        response = PTTaskCreateTaskingMessageResponse(
-            TaskID=taskData.Task.ID,
-            Success=True,
-        )
-        return response
+	async def create_go_tasking(self, taskData: PTTaskMessageAllData) -> PTTaskCreateTaskingMessageResponse:
+		response = PTTaskCreateTaskingMessageResponse(
+			TaskID=taskData.Task.ID,
+			Success=True,
+		)
+		return response
