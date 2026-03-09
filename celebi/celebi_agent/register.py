0f24ae7e2b0720f74cc852d5ea296985bcfc8153
Callum Murphy-Hale <19550999+ofasgard@users.noreply.github.com>
2026-03-09 12:46:14 +0000

    feat(Agent): Create register command

---
+++ celebi/celebi_agent/register.py
@@ -0,0 +1,47 @@
+from mythic_container.MythicCommandBase import *
+
+import base64
+
+class RegisterArguments(TaskArguments):
+
+	def __init__(self, command_line, **kwargs):
+		super().__init__(command_line, **kwargs)
+		self.args = [
+			CommandParameter(
+				name="file",
+				cli_name="file",
+				display_name="file",
+				type=ParameterType.File,
+				parameter_group_info=[ ParameterGroupInfo(required=True) ]
+			)
+		]
+
+	async def parse_arguments(self):
+		if len(self.command_line) == 0:
+			raise Exception("Please provide the path to a file to register.")
+		if self.command_line[0] != "{":
+			raise Exception("Require JSON blob, but got raw command line.")
+		self.load_args_from_json_string(self.command_line)
+		
+class RegisterCommand(CommandBase):
+	cmd = "register" # Name of the command
+	help_cmd = "register <file>" # Help information presented to the user
+	argument_class = RegisterArguments # The class used for processing & validating arguments
+	description = "Upload a file to the agent and load it into mapped memory."
+	needs_admin = False
+	version = 1
+	author = "@ofasgard"
+	attackmapping = []
+	supported_ui_features = []
+	attributes = CommandAttributes(
+		builtin=True, # Is this command always compiled into this payload type?
+		suggested_command=True, # Is this command preselected when building a payload?
+	)
+
+	async def create_go_tasking(self, taskData: PTTaskMessageAllData) -> PTTaskCreateTaskingMessageResponse:
+		response = PTTaskCreateTaskingMessageResponse(
+			TaskID=taskData.Task.ID,
+			Success=True,
+		)
+		
+		return response
--- celebi/main.py
+++ celebi/main.py
@@ -4,5 +4,6 @@ import asyncio
 import celebi_agent.celebi
 import celebi_agent.exit
 import celebi_agent.getuid
+import celebi_agent.register
 
 mythic_container.mythic_service.start_and_run_forever()
